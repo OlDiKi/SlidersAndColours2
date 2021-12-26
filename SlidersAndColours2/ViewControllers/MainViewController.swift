d34ad1c353b71f84fd5134b9d6b43eb58b70f148
OlDiKi <delorian101allgood@gmail.com>
2021-12-26 23:47:04 +0300

    ver 1.0

--- SlidersAndColours2/ViewControllers/MainViewController.swift
+++ SlidersAndColours2/ViewControllers/MainViewController.swift
@@ -7,13 +7,23 @@
 
 import UIKit
 
+protocol SettingsViewControllerDelegate {
+    func setNewColor(color: UIColor)
+}
+
 class MainViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let settingsVC = segue.destination as? SettingsViewController else {return}
+        settingsVC.delegate = self
+        settingsVC.color = view.backgroundColor
     }
+}
 
-
+extension MainViewController: SettingsViewControllerDelegate {
+    func setNewColor(color: UIColor) {
+        view.backgroundColor = color
+    }
+    
 }
 
--- SlidersAndColours2/ViewControllers/SettingsViewController.swift
+++ SlidersAndColours2/ViewControllers/SettingsViewController.swift
@@ -7,8 +7,11 @@
 
 import UIKit
 
+
 class SettingsViewController: UIViewController {
 
+    //MARK: - IBOutlets
+    
     @IBOutlet weak var colorView: UIView!
     
     @IBOutlet weak var redColorValue: UILabel!
@@ -19,45 +22,78 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var greenColorSlider: UISlider!
     @IBOutlet weak var blueColorSlider: UISlider!
     
-    @IBOutlet weak var redColorText: UITextField!
-    @IBOutlet weak var greenColorText: UITextField!
-    @IBOutlet weak var blueColorText: UITextField!
+    @IBOutlet weak var redColorTF: UITextField!
+    @IBOutlet weak var greenColorTF: UITextField!
+    @IBOutlet weak var blueColorTF: UITextField!
     
+    //MARK:  Properties
+    
+    var delegate: SettingsViewControllerDelegate!
+    var color: UIColor!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         colorView.layer.cornerRadius = 10
+        colorView.backgroundColor = color
         
         redColorSlider.minimumTrackTintColor = .red
         greenColorSlider.minimumTrackTintColor = .green
         
-        changeColor()
-        setValue(for: redColorValue, greenColorValue, blueColorValue)
-
+        setSliders()
+        setValueLabels()
+        setValueTextFields()
     }
     
+    //MARK: - IBAction's
+    
     @IBAction func doneButtonPressed(_ sender: UIButton) {
+      
+        let savedColor = colorView.backgroundColor!
+        delegate.setNewColor(color: savedColor)
         dismiss(animated: true)
     }
     
     @IBAction func rgbChanged(_ sender: UISlider) {
         changeColor()
         
+        let value = String(format: "%.2f", sender.value)
+        
         switch sender {
-        case redColorSlider: setValue(for: redColorValue)
-        case greenColorSlider: setValue(for: greenColorValue)
-        default: setValue(for: blueColorValue)
+        case redColorSlider:
+            redColorValue.text = value
+            redColorTF.text = value
+        case greenColorSlider:
+            greenColorValue.text = value
+            greenColorTF.text = value
+        default:
+            blueColorValue.text = value
+            blueColorTF.text = value
         }
     }
     
-//MARK: - Private methods
+}
+
+//MARK: - Private Methods
+
+extension SettingsViewController {
     
-private func changeColor() {
-    colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value), green: CGFloat(greenColorSlider.value), blue: CGFloat(blueColorSlider.value), alpha: 1)
+    private func changeColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redColorSlider.value),
+                                            green: CGFloat(greenColorSlider.value),
+                                            blue: CGFloat(blueColorSlider.value),
+                                            alpha: 1)
+        }
+    
+    private func setSliders() {
+        let newColor = CIColor(color: color)
+        
+        redColorSlider.value = Float(newColor.red)
+        greenColorSlider.value = Float(newColor.green)
+        blueColorSlider.value = Float(newColor.blue)
     }
     
-private func setValue(for values: UILabel...) {
+    private func setValueLabels(for values: UILabel...) {
     values.forEach { value in
         switch value {
         case redColorValue:
@@ -66,13 +102,68 @@ private func setValue(for values: UILabel...) {
             value.text = string(from: greenColorSlider)
         default:
             value.text = string(from: blueColorSlider)
+            }
         }
     }
-}
     
-private func string(from slider: UISlider) -> String {
+    private func setValueTextFields(for values: UITextField...) {
+        values.forEach { value in
+            switch value {
+            case redColorTF:
+                value.text = string(from: redColorSlider)
+            case greenColorTF:
+                value.text = string(from: greenColorSlider)
+            default:
+                value.text = string(from: blueColorSlider)
+            }
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
         String(format: "%.2f", slider.value)
     }
 }
 
+//MARK: - Alert
+
+extension SettingsViewController {
+    private func showAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        let okAction = UIAlertAction(title: "OK",
+                                     style: .default)
+        alert.addAction(okAction)
+        present(alert, animated: true)
+    }
+}
+
+// MARK: - UITextFieldDelegate
+
+extension SettingsViewController: UITextFieldDelegate {
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        guard let newValue = textField.text else { return }
+        guard let currentValue = Float(newValue) else { return
+            showAlert(title: "Wrong value!",
+                      message: "Please enter correct number from 0 to 1")
+        }
+        
+        if currentValue >= 0 && currentValue <= 1 {
+            switch textField.tag {
+            case 0: redColorSlider.value = currentValue
+            case 1: greenColorSlider.value = currentValue
+            case 2: blueColorSlider.value = currentValue
+            default: break
+            }
+            changeColor()
+            setValueLabels()
+            setValueTextFields()
+        } else {
+            showAlert(title: "Wrong value!",
+                      message: "Please enter correct number from 0 to 1")
+        }
+    }
+}
 
